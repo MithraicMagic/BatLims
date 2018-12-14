5349446289a2adc5f7ee3425346cdea3bdd80323
nickelbink@gmail.com <nickelbink@gmail.com>
2018-12-14 16:32:30 +0100

    Trying out some stuff in JS

--- Batlim/battle_limousines/game.js
+++ Batlim/battle_limousines/game.js
@@ -3,5 +3,10 @@ var game = function (gameID) {
     this.playerTwo = null;
     this.id = gameID;
     this.gameState = "0 PLAYR";
-}
+};
 
+var output = function (x) {
+    console.log("You know: " + x);
+};
+
+output("Death, is but a virtue");
