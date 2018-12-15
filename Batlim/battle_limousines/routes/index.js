3845c9621c53112a1cfbcd4515d26f8b09f7b2b2
nickelbink@gmail.com <nickelbink@gmail.com>
2018-12-15 13:19:32 +0100

    Changed routes so that the proper pages are shown on request.

--- Batlim/battle_limousines/routes/index.js
+++ Batlim/battle_limousines/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//Getting the homepage
+router.get('/', function(req, res) {
+  res.sendFile('splash.html', { root: "./public" });
+});
+
+//Getting the game page when pressing the start button
+router.get('/play', function(req, res) {
+  res.sendFile('game.html', { root: "./public"});
 });
 
 module.exports = router;
