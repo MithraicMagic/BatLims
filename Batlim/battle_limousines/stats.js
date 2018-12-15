235cb39f666150670147f25bacff958b4292a607
nickelbink@gmail.com <nickelbink@gmail.com>
2018-12-15 13:32:11 +0100

    Stats will now be recorded from start of server

---
+++ Batlim/battle_limousines/stats.js
@@ -0,0 +1,8 @@
+var gameStatus = {
+    since : Date.now(),
+    started : 0,   //number of games that have started
+    stopped : 0,  //number of games stopped (not finished)
+    finished : 0  //number of completed games
+};
+
+module.exports = gameStatus;
